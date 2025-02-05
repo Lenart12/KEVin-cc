54136a6b49a3f20ed9de97b5fb02f84f2ceb2503
Lenart Kos <koslenart@gmail.com>
2025-02-05 18:37:33 +0100

    Add secrets.yaml

--- config.py
+++ config.py
@@ -2,54 +2,68 @@ import yaml
 from datetime import datetime
 
 class Config:
-    def __init__(self, config_fn: str = 'config.yaml'):
+    def _get(self, *args):
+        result = self._config
+        for arg in args:
+            result = result[arg]
+        if isinstance(result, str):
+            if result.startswith('$secret '):
+                secret = result.split('$secret ')[-1]
+                result = self._config['_secrets'][secret]
+
+        return result
+
+    def __init__(self, config_fn: str = 'config.yaml', secrets_fn: str = 'secrets.yaml'):
         with open(config_fn, 'r') as f:
-            config = yaml.safe_load(f)
-        self.api_url = config['api']['url']
-        self.api_token = config['api']['token']
+            self._config = yaml.safe_load(f)
+        with open(secrets_fn, 'r') as f:
+            self._config['_secrets'] = yaml.safe_load(f)['secrets']
 
-        self.set_charging_entity_id = config['api']['entities']['switch_set_charging']
-        self.set_charging_amps_entity_id = config['api']['entities']['number_set_charging_amps']
-        self.charging_plan_entity_id = config['api']['entities']['input_select_charging_plan']
+        self.api_url = self._get('api', 'url')
+        self.api_token = self._get('api', 'token')
 
-        self.charging_amps_template = config['api']['templates']['charging_amps']
-        self.charging_limit_template = config['api']['templates']['charging_limit']
-        self.charging_plan_template = config['api']['templates']['charging_plan']
-        self.inverter_soc_template = config['api']['templates']['inverter_soc']
-        self.car_soc_template = config['api']['templates']['car_soc']
-        self.battery_load_template = config['api']['templates']['battery_load']
-        self.total_load_template = config['api']['templates']['total_load']
-        self.grid_power_template = config['api']['templates']['grid_power']
-        self.pv_power_template = config['api']['templates']['pv_power']
-        self.charger_connected_template = config['api']['templates']['charger_connected']
-        self.is_charging_template = config['api']['templates']['is_charging']
+        self.set_charging_entity_id = self._get('api', 'entities', 'switch_set_charging')
+        self.set_charging_amps_entity_id = self._get('api', 'entities', 'number_set_charging_amps')
+        self.charging_plan_entity_id = self._get('api', 'entities', 'input_select_charging_plan')
 
-        self.min_amps = config['charger']['min_amps']
-        self.max_amps = config['charger']['max_amps']
-        self.min_power = config['charger']['min_power']
-        self.max_power = config['charger']['max_power']
-        self.vehicle_battery_capacity = config['charger']['vehicle_battery_capacity']
-        self.phases = config['charger']['phases']
-        self.volts = config['charger']['volts']
-        self.poll_interval = config['charger']['poll_interval']
-        self.charge_efficiency_factor = config['charger']['charge_efficiency_factor']
+        self.charging_amps_template = self._get('api', 'templates', 'charging_amps')
+        self.charging_limit_template = self._get('api', 'templates', 'charging_limit')
+        self.charging_plan_template = self._get('api', 'templates', 'charging_plan')
+        self.inverter_soc_template = self._get('api', 'templates', 'inverter_soc')
+        self.car_soc_template = self._get('api', 'templates', 'car_soc')
+        self.battery_load_template = self._get('api', 'templates', 'battery_load')
+        self.total_load_template = self._get('api', 'templates', 'total_load')
+        self.grid_power_template = self._get('api', 'templates', 'grid_power')
+        self.pv_power_template = self._get('api', 'templates', 'pv_power')
+        self.charger_connected_template = self._get('api', 'templates', 'charger_connected')
+        self.is_charging_template = self._get('api', 'templates', 'is_charging')
+
+        self.min_amps = self._get('charger', 'min_amps')
+        self.max_amps = self._get('charger', 'max_amps')
+        self.min_power = self._get('charger', 'min_power')
+        self.max_power = self._get('charger', 'max_power')
+        self.vehicle_battery_capacity = self._get('charger', 'vehicle_battery_capacity')
+        self.phases = self._get('charger', 'phases')
+        self.volts = self._get('charger', 'volts')
+        self.poll_interval = self._get('charger', 'poll_interval')
+        self.charge_efficiency_factor = self._get('charger', 'charge_efficiency_factor')
         def time_to_seconds(time_str):
             t = datetime.strptime(time_str, '%H:%M')
             return t.hour * 3600 + t.minute * 60
-        self.nightly_start = time_to_seconds(config['charger']['nightly']['start'])
-        self.nightly_end = time_to_seconds(config['charger']['nightly']['end'])
+        self.nightly_start = time_to_seconds(self._get('charger', 'nightly', 'start'))
+        self.nightly_end = time_to_seconds(self._get('charger', 'nightly', 'end'))
 
-        self.battery_soc_no_charging = config['battery']['no_charging']['soc']
-        self.battery_soc_reserve   = config['battery']['reserve']['soc']
-        self.battery_power_reserve = config['battery']['reserve']['max_power']
-        self.battery_soc_peak_shaving_minimal   = config['battery']['peak_shaving_minimal']['soc']
-        self.battery_power_peak_shaving_minimal = config['battery']['peak_shaving_minimal']['max_power']
-        self.battery_power_peak_shaving = config['battery']['peak_shaving']['max_power']
+        self.battery_soc_no_charging = self._get('battery', 'no_charging', 'soc')
+        self.battery_soc_reserve = self._get('battery', 'reserve', 'soc')
+        self.battery_power_reserve = self._get('battery', 'reserve', 'max_power')
+        self.battery_soc_peak_shaving_minimal = self._get('battery', 'peak_shaving_minimal', 'soc')
+        self.battery_power_peak_shaving_minimal = self._get('battery', 'peak_shaving_minimal', 'max_power')
+        self.battery_power_peak_shaving = self._get('battery', 'peak_shaving', 'max_power')
 
-        self.mqtt_host = config['mqtt']['host']
-        self.mqtt_port = config['mqtt']['port']
-        self.mqtt_user = config['mqtt']['username']
-        self.mqtt_pass = config['mqtt']['password']
-        self.mqtt_discovery_topic = config['mqtt']['discovery_topic']
-        self.mqtt_discovery = config['mqtt']['discovery']
-        self.mqtt_availability_topic = config['mqtt']['discovery']['availability_topic']
+        self.mqtt_host = self._get('mqtt', 'host')
+        self.mqtt_port = self._get('mqtt', 'port')
+        self.mqtt_user = self._get('mqtt', 'username')
+        self.mqtt_pass = self._get('mqtt', 'password')
+        self.mqtt_discovery_topic = self._get('mqtt', 'discovery_topic')
+        self.mqtt_discovery = self._get('mqtt', 'discovery')
+        self.mqtt_availability_topic = self._get('mqtt', 'discovery', 'availability_topic')
